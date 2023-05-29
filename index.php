c08d53fb4bbe4a5829dd5af5800e28327138be43
Wendryl <wendryl10000@gmail.com>
2023-05-29 17:03:27 -0300

    fix xampp setup

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use ProgWeb\TodoWeb\Controllers\UserController;
 
-require '../bootstrap.php';
+require './bootstrap.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -11,16 +11,16 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
+$uri = explode('/api', $uri);
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-if($uri[1] == 'users') {
+if($uri[1] == '/users') {
     $userController = new UserController($dbConnection, $requestMethod, $userId);
     $userController->processRequest();
     return;
 }
 
-header("HTTP/1.1 200 Not Found");
+header("HTTP/1.1 200 Ok");
 echo json_encode(['message' => 'A API Gestor Web está funcionando corretamente!'], JSON_UNESCAPED_UNICODE);
 exit();
---
+++ src/Controllers/ActivityController.php
@@ -0,0 +1,7 @@
+<?php
+namespace ProgWeb\TodoWeb\Controllers;
+
+use ProgWeb\TodoWeb\Gateways\UserGateway;
+
+class UserController extends BaseController {
+}
